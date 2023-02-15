48a0fef87a9140819d23676b04bfc5144d6e33c2
Gabi Lemos <gabicarvalholemos@gmail.com>
2023-02-15 18:12:15 -0300

    first commit

---
+++ js/login.js
@@ -0,0 +1,21 @@
+const input = document.querySelector('.loginInput')
+const button = document.querySelector('.loginButton')
+const form = document.querySelector('.loginForm')
+
+const validateInput = ({ target }) => {
+  if (target.value.length > 3) {
+    button.removeAttribute('disabled')
+    return
+  }
+  button.setAttribute('disabled', '')
+}
+
+const handleSubmit = event => {
+  event.preventDefault()
+
+  localStorage.setItem('player', input.value)
+  window.location = 'pages/game.html'
+}
+
+input.addEventListener('input', validateInput)
+form.addEventListener('submit', handleSubmit)
